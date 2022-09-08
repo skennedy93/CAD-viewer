ea27ea44eb0bb34efa3b5e0f6a7f78078204bf49
skennedy37 <sean47900@gmail.com>
2022-09-08 15:58:17 -0500

    cad added

--- src/home.js
+++ src/home.js
@@ -8,16 +8,16 @@ function Home() {
   <div><img class="ame-image-1" src={window.location.origin + '/images/logo.png'}/></div>
 </div>
 <div class="view-container-home">
-<div class="cad-buttons">
+<div class="cad-buttons-home">
   <div class="row">
- <a href='/amrok'> <button type="button" class="button-basic"><img src={window.location.origin + '/images/amrok-logo.png'}/></button></a>
-  <a href='/miteebite'><button type="button" class="button-basic"><img src={window.location.origin + '/images/mitee-Bite.webp'}/></button></a>
-  <a href='/triag'><button type="button" class="button-basic"><img src={window.location.origin + '/images/triag.webp'}/></button></a>
+ <a href='/amrok'> <button type="button" class="button-basic"><img class="home-images" src={window.location.origin + '/images/amrok-logo.svg'}/></button></a>
+  <a href='/miteebite'><button type="button" class="button-basic"><img class="home-images" src={window.location.origin + '/images/miteebite-logo.svg'}/></button></a>
+  <a href='/triag'><button type="button" class="button-basic"><img class="home-images" src={window.location.origin + '/images/triag-logo.svg'}/></button></a>
   </div>
  <div class="row">
-<a href='/fifthaxis'><button type="button" class="button-basic"><img src={window.location.origin + '/images/axis-vises.webp'}/></button></a>
-<a href='/trinity'><button type="button" class="button-basic"><img src={window.location.origin + '/images/trinity.png'}/></button> </a>  
-<a href='/schunk'><button type="button" class="button-basic"><img src={window.location.origin + '/images/schunk.webp'}/></button></a> 
+<a href='/fifthaxis'><button type="button" class="button-basic"><img class="home-images" src={window.location.origin + '/images/fifth-axis-logo.svg'}/></button></a>
+<a href='/trinity'><button type="button" class="button-basic"><img class="home-images" src={window.location.origin + '/images/trinity-logo.svg'}/></button> </a>  
+<a href='/schunk'><button type="button" class="button-basic"><img class="home-images" src={window.location.origin + '/images/schunk-logo.svg'}/></button></a> 
  </div>
 
 </div>
--- src/partners/schunk.js
+++ src/partners/schunk.js
@@ -1,33 +1,53 @@
+import $ from "jquery";
 
 function firstcad() {
     var linkone = ['https://www.ame.com/assets/052022-TrinityAutomation-Pyramid.11/052022-TrinityAutomation-Pyramid.11.html']
       document.getElementById('myIframe').src = linkone;
+      $(".menu").toggleClass("active");
+      $(".menu2").removeClass("active");
+      $(".menu3").removeClass("active");
+      $(".button-container1").toggleClass("overlay1");
+      $(".button-container2").addClass("overlay2");
+      $(".button-container3").addClass("overlay3");
   }    
   function secondcad() {
     var linktwo = ['https://www.ame.com/assets/AMROK Subplate - Triag - Sample 1 - v2.15/AMROK Subplate - Triag - Sample 1 - v2.15.html']
     document.getElementById('myIframe').src = linktwo;
+    $(".menu2").toggleClass("active");
+    $(".menu").removeClass("active");
+    $(".menu3").removeClass("active");
+    $(".button-container2").toggleClass("overlay2");
+    $(".button-container1").addClass("overlay1");
+    $(".button-container3").addClass("overlay3");
+  }   
+  function thirdcad() {
+    var linkthree = ['https://www.ame.com/assets/AMROK Subplate - Triag - Sample 1 - v2.15/AMROK Subplate - Triag - Sample 1 - v2.15.html']
+    document.getElementById('myIframe').src = linkthree;
+    $(".menu3").toggleClass("active");
+    $(".menu").removeClass("active");
+    $(".menu2").removeClass("active");
+    $(".button-container3").toggleClass("overlay3");
+    $(".button-container1").addClass("overlay1");
+    $(".button-container2").addClass("overlay2");
   }   
   function Schunk() {
   
     return (
   <>
 <div class="navbar">
-  <div><img class="ame-image-1" src={window.location.origin + '/images/schunk.webp'}/></div>
+<div><img class="ame-image-1" src={window.location.origin + '/images/BOTR.png'}/></div>
+  <div class="divider">|</div>
+  <div><img class="amrok" src={window.location.origin + '/images/schunk-white.svg'}/></div>
 </div>
   <div class="view-container">
+
   <div class="cad-buttons">
-    <div class="row">
-    <button type="button" class="button-basic" value="Pyramid" onClick={firstcad}><img src={window.location.origin + '/images/button-1.jpg'}/></button>
-    <button type="button" class="button-basic" value="Subplate" onClick={secondcad}><img src={window.location.origin + '/images/button-2.jpg'}/></button>
-    </div>
-   <div class="row">
-  <button type="button" class="button-basic" value="Pyramid" onClick={firstcad}><img src={window.location.origin + '/images/button-1.jpg'}/></button>
-  <button type="button" class="button-basic" value="Subplate" onClick={secondcad}><img src={window.location.origin + '/images/button-2.jpg'}/></button>   
-   </div>
-  
+    <div class="button-container1 overlay1"><button type="button" class="button-basic" value="Pyramid" onClick={firstcad}><img src={window.location.origin + '/images/button-1.jpg'}/></button></div><div class="menu">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</div>
+    <div class="button-container2 overlay2"><button type="button" class="button-basic" value="Subplate" onClick={secondcad}><img src={window.location.origin + '/images/button-2.jpg'}/></button></div><div class="menu2">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</div>
+    <div class="button-container3 overlay3"><button type="button" class="button-basic" value="Pyramid" onClick={thirdcad}><img src={window.location.origin + '/images/button-1.jpg'}/></button></div> <div class="menu3">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</div>
   </div>
   <div class="iframe-viewer">
-  <iframe id="myIframe" src="https://www.ame.com/assets/052022-TrinityAutomation-Pyramid.11/052022-TrinityAutomation-Pyramid.11.html" width="590" height="500" frameBorder="0" ></iframe>
+  <iframe id="myIframe" src="https://www.ame.com/assets/052022-TrinityAutomation-Pyramid.11/052022-TrinityAutomation-Pyramid.11.html" width="520" height="520" frameBorder="0" ></iframe>
   </div>
   </div>
   <div class="footer"><div class="footer-button">REQUEST CATALOGS OR MORE INFORMATION AT THE DESK</div></div>
